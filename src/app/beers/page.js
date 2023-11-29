738463da39bfa6a21d78791e3bf30ba644ad415f
Chris <chris.catignani@gmail.com>
2023-11-29 10:54:48 +0100

    prevent /beers route from loading without a name

--- src/app/beers/page.js
+++ src/app/beers/page.js
@@ -1,12 +1,19 @@
 'use client'
 
+import { useRouter } from 'next/navigation';
 import { Box } from "@chakra-ui/react";
 import { Beers } from "./Beers";
 import { useSearchParams } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter()
   const searchParams = useSearchParams()
-  const name = searchParams.get('name')
+  const name = searchParams.get('name') || ''
+
+  if (!name) {
+    router.push('/')
+    return (<></>)
+  }
 
   return (
     <main>
