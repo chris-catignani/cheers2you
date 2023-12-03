d3ebb0e49ab6749fa41eb0aa8bcf2a085fd2b6ec
Chris <chris.catignani@gmail.com>
2023-12-03 15:50:06 -0500

    Tell  browsers to cache proxied images for 6 hours

--- src/app/beers/api/route.js
+++ src/app/beers/api/route.js
@@ -1,7 +1,9 @@
 
+const SIX_HOURS_AS_SECONDS = 6 * 60 * 60
+
 // Proxy for downloading images
 export async function GET(req) {
     const { searchParams } = new URL(req.url)
     const resp = await fetch(searchParams.get('url'))
-    return new Response(resp.body)
+    return new Response(resp.body, { headers: {'Cache-Control': `max-age=${SIX_HOURS_AS_SECONDS}`}})
 }
