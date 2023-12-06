38fed01812e7a7a214f1f49bf011465718bd57df
Chris <chris.catignani@gmail.com>
2023-12-06 18:33:36 -0500

    Bump image proxy browser cache to one day

--- src/app/beers/api/imageProxy/route.js
+++ src/app/beers/api/imageProxy/route.js
@@ -1,9 +1,9 @@
 
-const SIX_HOURS_AS_SECONDS = 6 * 60 * 60
+const ONE_DAY_AS_SECONDS = 24 * 60 * 60
 
 // Proxy for downloading images
 export async function GET(req) {
     const { searchParams } = new URL(req.url)
     const resp = await fetch(searchParams.get('url'))
-    return new Response(resp.body, { headers: {'Cache-Control': `max-age=${SIX_HOURS_AS_SECONDS}`}})
+    return new Response(resp.body, { headers: {'Cache-Control': `max-age=${ONE_DAY_AS_SECONDS}`}})
 }
