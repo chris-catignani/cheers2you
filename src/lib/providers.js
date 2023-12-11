287765bcbb10db365539567c481bfc59b7412393
Chris <chris.catignani@gmail.com>
2023-12-11 10:44:06 -0500

    fixing fonts

--- src/lib/providers.js
+++ src/lib/providers.js
@@ -1,11 +1,21 @@
 'use client'
 
-/* Core */
+import "@fontsource-variable/inter"; // Defaults to wght axis
+import "@fontsource-variable/inter/wght.css"; // Specify axis
+import "@fontsource-variable/inter/slnt.css";
+import "@fontsource-variable/inter/standard.css";
+
+import "@fontsource-variable/inter-tight"; // Defaults to wght axis
+import "@fontsource-variable/inter-tight/wght.css"; // Specify axis
+import "@fontsource-variable/inter-tight/wght-italic.css"; // Specify axis and style
+
+import "@fontsource-variable/familjen-grotesk"; // Defaults to wght axis
+import "@fontsource-variable/familjen-grotesk/wght.css"; // Specify axis
+import "@fontsource-variable/familjen-grotesk/wght-italic.css"; // Specify axis and style
+
 import { Provider } from 'react-redux'
 import { ChakraProvider } from '@chakra-ui/react'
 import { CacheProvider } from '@chakra-ui/next-js'
-
-/* Instruments */
 import { reduxStore } from '@/lib/redux'
 import { cheers2YouTheme } from '@/app/chakra/theme/theme'
 
