ba10ab688a992c2252d2da06c35bf497f745a6e7
ReginaldMolehusband <152019121+ReginaldMolehusband@users.noreply.github.com>
2023-12-13 14:00:53 +0000

    Corrected Janet Plaque

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -49,7 +49,7 @@ export const Home = ({venueName}) => {
                 </Box>
                 <Box border='2px' borderColor='lightgray'>
                     <Image
-                        src='/janet-plaque3png'
+                        src='/janet-plaque3.png'
                         alt='Janet beer banner example'
                         htmlWidth={800}
                         htmlHeight={500}/>
