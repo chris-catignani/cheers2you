5dfb3ea0e59e445099e42853e56474b73308f9cb
Chris <chris.catignani@gmail.com>
2023-12-18 13:27:56 -0500

    don't crash if no matching beer found

--- src/app/beers/components/SlotMachine.js
+++ src/app/beers/components/SlotMachine.js
@@ -100,8 +100,8 @@ const BeerImages = ({ beers, size }) => {
     const buildImage = ({beer}, idx) => (
         <Image
             key={`slot-reel-${idx}-option-${idx}`}
-            src={beer.beer_label_file}
-            alt={beer.beer_name + ' ' + beer.beer_type}
+            src={beer?.beer_label_file}
+            alt={beer?.beer_name + ' ' + beer?.beer_type}
             boxSize={size}
             my='40px'
             fit='contain' />
