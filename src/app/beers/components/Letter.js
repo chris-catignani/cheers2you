de35ade18b9e441fd6803f0a20d5b191660d0b30
Chris <chris.catignani@gmail.com>
2023-12-17 19:25:33 -0500

    Revert "Revert "Temporarily truncate text instead of having ellipses""
    This reverts commit 575f7102fb7c340fed9bbf63cbfa914483e91fd1.

--- src/app/beers/components/Letter.js
+++ src/app/beers/components/Letter.js
@@ -17,11 +17,17 @@ export const Letter = ({ beer, onClick, matchedFields = [] } = {}) => {
         beerTypeProps['fontWeight'] = 'bold'
     }
 
+    /**
+     * TODO ellipses if truncation.
+     * Box has a method noOfLines which uses webkit-box under the hood.
+     * our library which captures the images does not support webkit-box. So it renders funny in downloads/screenshots
+     */
+
     return (
         <Box paddingTop='2' paddingBottom='2' textAlign='center' onClick={onClick} fontFamily={`'Inter Tight Variable', sans-serif`}>
-            <Box noOfLines='2' overflow='hidden' lineHeight='1.1em' height='2.3em' {...brewerNameProps}>{beer?.brewer_name}</Box>
-            <Box noOfLines='2' overflow='hidden' lineHeight='1.1em' height='2.3em' marginTop='2' {...beerNameProps}>{beer?.beer_name}</Box>
-            <Box noOfLines='2' overflow='hidden' lineHeight='1.1em' height='2.3em' marginTop='2' {...beerTypeProps}>{beer?.beer_type}</Box>
+            <Box maxHeight='2.3em' overflow='hidden' lineHeight='1.1em' height='2.3em' {...brewerNameProps}>{beer?.brewer_name}</Box>
+            <Box maxHeight='2.3em' overflow='hidden' lineHeight='1.1em' height='2.3em' marginTop='2' {...beerNameProps}>{beer?.beer_name}</Box>
+            <Box maxHeight='2.3em' overflow='hidden' lineHeight='1.1em' height='2.3em' marginTop='2' {...beerTypeProps}>{beer?.beer_type}</Box>
         </Box>
     )
 }
