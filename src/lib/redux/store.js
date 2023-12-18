a9c66310ef309fd47acae56431f088064e7808ee
Chris <chris.catignani@gmail.com>
2023-12-18 15:37:04 -0500

    add back in redux-logger

--- src/lib/redux/store.js
+++ src/lib/redux/store.js
@@ -3,7 +3,11 @@ import { configureStore } from '@reduxjs/toolkit'
 
 /* Instruments */
 import { reducer } from './rootReducer'
+import { middleware } from './middleware'
 
 export const reduxStore = configureStore({
   reducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(middleware)
+  },
 })
